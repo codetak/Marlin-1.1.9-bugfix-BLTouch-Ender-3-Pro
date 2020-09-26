e3630d797ab3248cb389ae7205d5b3e6a3fcc31a
codetak <liberotak@gmail.com>
2020-09-26 18:28:23 +0300

    Z_PROBE_LOW_POINT comments added

--- Marlin-bugfix-1.1.x/Marlin/Configuration.h
+++ Marlin-bugfix-1.1.x/Marlin/Configuration.h
@@ -817,7 +817,7 @@
 #define Z_CLEARANCE_MULTI_PROBE     5 // Z Clearance between multiple probes
 //#define Z_AFTER_PROBING           5 // Z position after probing is done
 
-#define Z_PROBE_LOW_POINT          -2 // Farthest distance below the trigger-point to go before stopping
+#define Z_PROBE_LOW_POINT          -2 // Farthest distance below the trigger-point to go before stopping - This is the maximum deviation to go from z_home_pos found
 
 // For M851 give a range for adjusting the Z probe offset
 #define Z_PROBE_OFFSET_RANGE_MIN -20
