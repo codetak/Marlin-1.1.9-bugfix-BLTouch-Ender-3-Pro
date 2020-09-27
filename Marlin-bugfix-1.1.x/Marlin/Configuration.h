4a4e146dda7fd5c7725591278c229e432c06e136
codetak <liberotak@gmail.com>
2020-09-27 18:38:08 +0300

    Changed default z_probe_offset

--- Marlin-bugfix-1.1.x/Marlin/Configuration.h
+++ Marlin-bugfix-1.1.x/Marlin/Configuration.h
@@ -779,7 +779,7 @@
  */
 #define X_PROBE_OFFSET_FROM_EXTRUDER -43  // X offset: -left  +right  [of the nozzle]
 #define Y_PROBE_OFFSET_FROM_EXTRUDER -9  // Y offset: -front +behind [the nozzle]
-#define Z_PROBE_OFFSET_FROM_EXTRUDER -2.85   // Z offset: -below +above  [the nozzle] - Greater value moves nozzle close to the bed
+#define Z_PROBE_OFFSET_FROM_EXTRUDER -2.87   // Z offset: -below +above  [the nozzle] - Greater(negative) value moves nozzle close to the bed
 
 // Certain types of probes need to stay away from edges
 #define MIN_PROBE_EDGE 50 // Default is 20 - It must be greater than or equal to the higher of the X_PROBE_OFFSET_FROM_EXTRUDER and Y_PROBE_OFFSET_FROM_EXTRUDER
